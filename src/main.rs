d8296317c333f57de656b3838ebbf77fbdde58fd
drawhale <kdw115@naver.com>
2025-09-24 17:18:36 +0900

    feat: study trait

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,12 @@
+use rust_example::{Summary, Tweet};
+
 fn main() {
-    let guess = Guess::new(200);
-    println!("{}", guess.value());
-}
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
 
-pub struct Guess {
-    value: i32,
-}
-
-impl Guess {
-    pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("Guess value must be between 1 and 100, got {}.", value);
-        }
-
-        Guess { value }
-    }
-
-    pub fn value(&self) -> i32 {
-        self.value
-    }
+    println!("1 new tweet: {}", tweet.summarize());
 }
