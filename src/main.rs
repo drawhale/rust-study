e8ba4edf65aa7441e52046a54959d6fe816c6095
drawhale <kdw115@naver.com>
2025-10-01 18:49:02 +0900

    feat: study minigrep (wip)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,37 @@
-use rust_example::{Summary, Tweet};
+use std::env;
+use std::fs;
+use std::process;
 
 fn main() {
-    let tweet = Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from("of course, as you probably already know, people"),
-        reply: false,
-        retweet: false,
-    };
+  let args: Vec<String> = env::args().collect();
+  dbg!(&args);
 
-    println!("1 new tweet: {}", tweet.summarize());
+  let config = Config::parse(&args).unwrap_or_else(|err| {
+    println!("Problem parsing arguments: {}", err);
+    process::exit(1);
+  });
+  
+  println!("Searching for {}", config.query);
+  println!("In file {}", config.file_path);
+
+  let contents = fs::read_to_string(config.file_path)
+  .expect("Should have been able to read the file");
+  println!("With text:\n{}", contents);
+}
+
+struct Config {
+  query: String,
+  file_path: String,
+}
+
+impl Config {
+  fn parse(args: &[String]) -> Result<Config, &'static str> {
+    if args.len() < 3 {
+      return Err("Not enough arguments");
+    }
+
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    Ok(Config { query, file_path })
+  }
 }
