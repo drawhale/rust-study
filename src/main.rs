93a46951e918e35cd60fac41340400b224c3a6f2
drawhale <kdw115@naver.com>
2025-10-28 16:26:02 +0900

    feat: study minigrep-improve

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,19 @@ use std::process;
 use rust_example::Config;
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
-  dbg!(&args);
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
 
-  let config = Config::parse(&args).unwrap_or_else(|err| {
-    println!("Problem parsing arguments: {}", err);
-    process::exit(1);
-  });
-  
-  println!("Searching for {}", config.query);
-  println!("In file {}", config.file_path);
+    let config = Config::parse(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-  if let Err(e) = rust_example::run(config) {
-    println!("Application error: {}", e);
-    process::exit(1)
-  }
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = rust_example::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1)
+    }
 }
