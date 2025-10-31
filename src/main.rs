0c5476d8ac662ab9d5ca66800355cb26f2d41ebe
drawhale <kdw115@naver.com>
2025-10-31 13:46:14 +0900

    feat: study deref

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,35 @@
-use std::env;
-use std::process;
+use std::ops::Deref;
 
-use rust_example::Config;
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn hello(name: &String) {
+    println!("Hello, {name}!");
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    dbg!(&args);
+    let x = 5;
+    let y = Box::new(x);
 
-    let config = Config::parse(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    let m = MyBox::new(x);
+    assert_eq!(5, *m);
 
-    if let Err(e) = rust_example::run(config) {
-        eprintln!("Application error: {}", e);
-        process::exit(1)
-    }
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
 }
